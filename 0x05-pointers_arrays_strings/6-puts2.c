a5dfc82008fcef966a05438ed9b764fcc7f32a39
Poppablack <113602784+Poppablack@users.noreply.github.com>
2022-10-25 10:32:28 +0100

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
 /**
- * puts2 - Prints one char out of two of a string.
+ * puts2 - Prints one char out of 2 of a string.
+ * followed by a new line
  * @str: The string containing characters.
  */
 void puts2(char *str)
 {
-	int i, len = 0;
+	int len, i;
 
-	while (str[i++])
+    len = 0;
+
+	while (str[len]) != '\0')
+    {
 		len++;
+    }
 
 	for (i = 0; i < len; i += 2)
+    {
 		_putchar(str[i]);
+    }
 
 	_putchar('\n');
 }
